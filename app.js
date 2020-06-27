866544ce22f1e474c6e120d8d2f7a6c2efa9392b
saad shakil <58229518+2000ss-design@users.noreply.github.com>
2020-06-27 09:36:23 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,125 @@
+// var i;
+// for (var i=0 ; i<a ; i++){
+
+// var a = [i];
+
+// a[i]=i+1;
+
+// function caall(g){
+//         alert("fuction is called " + a[i] + "time");
+
+// }
+// caall(i);
+// }
+
+
+///////////////////////////////////////////////
+var result;
+function getnum(num){
+        result=document.getElementById("inputt");
+        result.value +=num;
+}
+
+function clearresult(){
+       result=document.getElementById("inputt")
+        result.value=" ";
+}
+
+function get_result(){
+        result=document.getElementById("inputt");
+        result.value = eval(result.value);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
